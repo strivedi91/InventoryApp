a23604a4f883c54054a77124697dfc264c243d4d
technicius <42804548+technicius@users.noreply.github.com>
2018-09-12 21:16:57 +0530

    Added Edmx Files

---
+++ InventoryApp/Controllers/API/BaseAPIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace InventoryApp.Controllers.API
+{
+    /// <summary>
+    /// Base API Controller
+    /// </summary>
+    public class BaseAPIController : ApiController
+    {
+        protected IHttpActionResult GetOkResult<T>(T result)
+        {
+            if (result == null)
+            {
+                return Ok();
+            }
+
+            return Ok(result);
+        }
+
+        protected IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+	}
+}
