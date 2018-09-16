6f00d989d03df7c12e6cedb271dd323fea1eb81d
technicius <42804548+technicius@users.noreply.github.com>
2018-09-16 12:24:29 +0530

    Added the dashboard page

--- InventoryApp/Areas/Admin/Controllers/HomeController.cs
+++ InventoryApp/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using InventoryApp.Areas.Admin.Models;
+using InventoryApp.Models;
+using InventoryApp_DL.Entities;
+using InventoryApp_DL.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +14,18 @@ namespace InventoryApp.Areas.Admin.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }        
+            (List<AspNetUsers>, int) objUsers = Repository<AspNetUsers>.GetEntityListForQuery(null);
+            (List<Products>, int) objProducts = Repository<Products>.GetEntityListForQuery(null);
+            (List<Categories>, int) objCategories = Repository<Categories>.GetEntityListForQuery(null);
+            (List<Orders>, int) objOrders = Repository<Orders>.GetEntityListForQuery(null);
+
+            return View(new DashboardModel
+            {
+                CategoryCount = objCategories.Item2,
+                OrderCount = objOrders.Item2,
+                ProductCount = objProducts.Item2,
+                SellerCount = objUsers.Item2
+            });
+        }
     }
 }
---
+++ InventoryApp/Areas/Admin/Models/DashboardModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryApp.Areas.Admin.Models
+{
+    public class DashboardModel
+    {
+        public int SellerCount { get; set; }
+        public int ProductCount { get; set; }
+        public int CategoryCount { get; set; }
+        public int OrderCount { get; set; }
+    }
+}
