0d002960a73b23a01293f9435ed0cb1f448cfe91
technicius <42804548+technicius@users.noreply.github.com>
2018-09-16 13:28:27 +0530

    Added Order details page

---
+++ InventoryApp/Areas/Admin/Controllers/OrderDetailsController.cs
@@ -0,0 +1,50 @@
+﻿using InventoryApp.Areas.Admin.Models;
+using InventoryApp_DL.Entities;
+using InventoryApp_DL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryApp.Areas.Admin.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        // GET: Admin/OrderDetails
+        public ActionResult Index(int OrderId)
+        {
+            List<Expression<Func<OrderDetails, Object>>> includes = new List<Expression<Func<OrderDetails, object>>>();
+            Expression<Func<OrderDetails, object>> IncludeProducts = (orderdetails) => orderdetails.Products;
+            Expression<Func<OrderDetails, object>> IncludeCategories = (orderdetails) => orderdetails.Categories;
+            includes.Add(IncludeProducts);
+            includes.Add(IncludeCategories);
+
+            var userOrders = Repository<OrderDetails>.
+                GetEntityListForQuery(x => x.OrderId == OrderId, null, includes).Item1;
+
+            List<ProductModel> productModels = new List<ProductModel>();
+            OrderDetailsModel orderDetails = new OrderDetailsModel();
+            foreach (var product in userOrders)
+            {
+                ProductModel productsModel = new ProductModel
+                {
+                    Brand = product.Products.Brand,
+                    Description = product.Products.Description,
+                    Discount = product.Discount,
+                    Name = product.Products.Name,
+                    Price = product.Price,
+                    TotalPrice = product.TotalPrice,
+                    Type = product.Products.Type,
+                    Category = product.Categories.Name,
+                    Quantity = product.Quantity
+                };
+
+                productModels.Add(productsModel);
+            }
+            orderDetails.Products = productModels;
+            return View(orderDetails);
+        }
+    }
+}
---
+++ InventoryApp/Areas/Admin/Models/OrderDetailsModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryApp.Areas.Admin.Models
+{
+    public class OrderDetailsModel
+    {
+        public List<ProductModel> Products { get; set; }
+    }
+
+    public class ProductModel
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
